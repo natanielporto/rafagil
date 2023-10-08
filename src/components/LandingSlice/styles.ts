f133af0ad52f6ae48c8b2b9c98ac1d0df4bef7cb
Nataniel Porto <natanielkporto@gmail.com>
2023-10-08 17:49:05 +0100

    add cellphone break

--- src/components/LandingSlice/styles.ts
+++ src/components/LandingSlice/styles.ts
@@ -1,59 +1,52 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  flex-direction: column-reverse;
+  position: relative;
   display: flex;
-
-  @media only screen and (min-width: 360px) {
-    flex-direction: column-reverse;
+  @media only screen and (min-width: 820px) {
   }
 `;
 
 export const LeftContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  width: 50%;
+  width: 100%;
+  height: 300px;
 
-  @media only screen and (min-width: 360px) {
-    width: 100%;
-    height: 300px;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-  }
   @media only screen and (min-width: 820px) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    width: 50%;
     margin-right: 2rem;
   }
 `;
 
 export const Logo = styled.img`
-  margin-right: 10rem;
-  @media only screen and (min-width: 360px) {
-    transform: scale(0.5);
-    position: absolute;
-    top: -100px;
-    left: 700px;
-  }
+  transform: scale(0.5);
+  position: absolute;
+  top: -100px;
+  left: 700px;
+
   @media only screen and (min-width: 820px) {
     transform: scale(0.6);
+    margin-right: 10rem;
     margin-right: 5rem;
     padding-right: 1rem;
   }
 `;
 
 export const MainTitle = styled.span`
-  margin-right: 5rem;
-  width: 60%;
+  position: absolute;
+  left: 120px;
   font-family: "Staatliches";
   color: ${(props) => props.theme.secondary};
-  font-size: 3.5rem;
-  @media only screen and (min-width: 360px) {
-    position: absolute;
-    left: 120px;
-    width: 280%;
-    font-size: 5.5rem;
-  }
+  font-size: 5.5rem;
+  margin-top: 5rem;
+
   @media only screen and (min-width: 820px) {
+    margin-right: 5rem;
+    width: 60%;
+    font-size: 3.5rem;
     margin-right: 0rem;
     width: 60%;
   }
@@ -66,11 +59,8 @@ export const RightContainer = styled.div`
 `;
 
 export const Image = styled.img`
+  width: 100%;
   height: 100%;
-  @media only screen and (min-width: 360px) {
-    width: 100%;
-    height: 100%;
-  }
   @media only screen and (min-width: 820px) {
     height: 100vh;
     width: 100%;
