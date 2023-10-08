7a7a0ab1da8b0d2780d9a50b77987f1c42f6267e
Nataniel Porto <natanielkporto@gmail.com>
2023-10-08 15:21:51 +0100

    small css change

--- src/components/ValuesSlice/styles.ts
+++ src/components/ValuesSlice/styles.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ValueContainer = styled.div`
   height: 100vh;
+  width: 100%;
   display: flex;
   justify-content: space-between;
   gap: 3rem;
