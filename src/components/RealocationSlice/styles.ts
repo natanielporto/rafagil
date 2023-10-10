edfeea088e04cb68eb8d17cfca444267612a08de
Renato Hideki Osaka <renato.osaka@hotmail.com>
2023-10-10 15:27:00 -0300

    fix: Grid template on relocation slice

--- src/components/RealocationSlice/styles.ts
+++ src/components/RealocationSlice/styles.ts
@@ -2,10 +2,10 @@ import styled from "styled-components";
 
 export const RelocationContainer = styled.div`
     display: grid;
-    grid-template-rows: 1fr;
+    grid-template-columns: 1fr;
 
     @media screen and (min-width: 768px) {
-      grid-template-columns: 1fr;
+      grid-template-columns: 1fr 1fr;
       gap: 1rem;
     }
 `;
